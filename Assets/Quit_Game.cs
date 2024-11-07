b2ad1d39fa8a5b68c6f74b5fc9c0fa2f068aa901
ankmandwal <ankmandwal@gmail.com>
2024-11-07 12:20:57 -0500

    Added a quit game script
    07_11_24_12_20_53

---
+++ Assets/Quit_Game.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Game : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
